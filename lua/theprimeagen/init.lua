273a0ddf6d5cf38d979bfa0ebbbcb582e960f7ee
PDSSOFTWARE\jwheeler <jwheeler@pdssoftware.com>
2024-04-18 17:38:44 -0500

    More changes to custom functions for PDS work

--- lua/theprimeagen/init.lua
+++ lua/theprimeagen/init.lua
@@ -5,18 +5,41 @@ if not vim.g.vscode then
     require("theprimeagen.packer")
 end
 
--- Define a function to save changes and copy the file to the desired location
-function SaveAndCopy()
-    -- Save changes if this isn't running in VSCode
-    if not vim.g.vscode then
-        vim.cmd(":w")
+conkc_test_path = '\\\\qa-web\\PDS\\vista\\conkc\\Web\\'
+pacy_test_path = '\\\\hostweb2\\PDS\\vista\\PACY\\PACYTEST\\Web\\'
+kcdc_test_path = '\\\\pds-web02\\PDS\\Vista\\KCDC\\KCDCTEST\\Web\\'
+bhs_test_path = '\\\\hostweb2\\PDS\\Vista\\BHS\\BHSDEVL\\Web\\'
+cpu_test_path = '\\\\pds-web03\\PDS\\Vista\\CPU\\CPUTEST\\Web\\'
+remote_path = pacy_test_path
+
+function CheckoutFromMain(commit_id)
+    -- Determine the HEAD of the main branch if commit_id is not provided
+    if commit_id == nil or commit_id == '' then
+        -- Get the HEAD of the 'main' branch using git command
+        local handle = io.popen('git rev-parse main')
+        commit_id = handle:read("*a"):gsub("\n", "")
+        handle:close()
     end
 
-    local kcdc_test_path = '\\\\pds-web02\\PDS\\Vista\\KCDC\\KCDCTEST\\Web\\'
-    local bhs_test_path = '\\\\hostweb2\\PDS\\Vista\\BHS\\BHSDEVL\\Web\\'
-    local cpu_test_path = '\\\\pds-web03\\PDS\\Vista\\CPU\\CPUTEST\\Web\\'
-    local remote_path = bhs_test_path
+    -- Get the current file path
+    local current_file = vim.fn.expand('%:p')
 
+    -- Print the command for debugging (optional)
+    print("Checking out file from commit ID: " .. commit_id)
+    print("Current file path: " .. current_file)
+
+    -- Execute the git checkout command to checkout the file from the specified commit ID
+    local command = string.format('git checkout %s -- "%s"', commit_id, current_file)
+    print(command)
+    local result = vim.fn.system(command)
+
+    -- Print the result of the git checkout command (optional)
+    print(result)
+
+    vim.cmd(":e!")
+end
+
+function GetCurrentFileAndDestinationPath()
     -- Get the current file path
     local current_file = vim.fn.expand('%:p')
     current_file = current_file:gsub('/', '\\')
@@ -25,15 +48,33 @@ function SaveAndCopy()
     local destination = current_file
     destination = destination:gsub('C:\\source\\repos\\technical%-consulting%-vista%-web\\', remote_path)
 
-    -- Print the command
-    --print("Copying file from: " .. current_file .. " to: " .. destination)
+    return current_file, destination
+end
+
+function TestPath()
+    local current_file, destination = GetCurrentFileAndDestinationPath()
+    
+    -- Check if the destination file exists
+    local file_exists = vim.fn.filereadable(destination) == 1
+    
+    -- Print the destination path and whether the file exists
+    print(destination, file_exists and "exists" or "does not exist")end
+
+-- Define a function to save changes and copy the file to the desired location
+function SaveAndCopy()
+    -- Save changes if this isn't running in VSCode
+    if not vim.g.vscode then
+        vim.cmd(":w")
+    end
+
+    local current_file, destination = GetCurrentFileAndDestinationPath()
 
     -- Copy the file to the destination
     vim.cmd("!copy \"" .. current_file .. "\" \"" .. destination .. "\"")
 end
 
 -- Define a function to save changes and copy the file to the desired location
-function SaveAndCopyToTemp()
+function TempSaveAndCopyTo()
     -- Save changes if this isn't running in VSCode
     if not vim.g.vscode then
         vim.cmd(":w")
@@ -65,4 +106,6 @@ end
 
 -- Define a custom command to call the function
 vim.cmd(":command! SaveAndCopy lua SaveAndCopy()")
-vim.cmd(":command! SaveAndCopyToTemp lua SaveAndCopyToTemp()")
+vim.cmd(":command! TempSaveAndCopyTo lua TempSaveAndCopyTo()")
+vim.cmd(":command! TestPath lua TestPath()")
+vim.cmd(":command! CheckoutFromMain lua CheckoutFromMain(<f-args>)")
